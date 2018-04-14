f80d8f7511b91eb2257445baf0f3bc930d3a4298
Anonymint <mint.com@gmail.com>
2018-04-14 12:39:38 -0500

    Finish project after debugging and test for a while

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -20,10 +20,6 @@
 using namespace std;
 
 void ParticleFilter::init(double x, double y, double theta, double std[]) {
-	// TODO: Set the number of particles. Initialize all particles to first position (based on estimates of 
-	//   x, y, theta and their uncertainties from GPS) and all weights to 1. 
-	// Add random Gaussian noise to each particle.
-	// NOTE: Consult particle_filter.h for more information about this method (and others in this file).
 	num_particles = 100;
 
 	//inital random and norm distribution
@@ -48,10 +44,6 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 }
 
 void ParticleFilter::prediction(double delta_t, double std_pos[], double velocity, double yaw_rate) {
-	// TODO: Add measurements to each particle and add random Gaussian noise.
-	// NOTE: When adding noise you may find std::normal_distribution and std::default_random_engine useful.
-	//  http://en.cppreference.com/w/cpp/numeric/random/normal_distribution
-	//  http://www.cplusplus.com/reference/random/default_random_engine/
 	std::default_random_engine gen;
 
 	for (int i=0; i < num_particles; i++) {
@@ -79,29 +71,11 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
 	}
 }
 
-void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::vector<LandmarkObs>& observations) {
-	// TODO: Find the predicted measurement that is closest to each observed measurement and assign the 
-	//   observed measurement to this particular landmark.
-	// NOTE: this method will NOT be called by the grading code. But you will probably find it useful to 
-	//   implement this method and use it as a helper during the updateWeights phase.
-
-}
-
 void ParticleFilter::updateWeights(double sensor_range, double std_landmark[], 
 		const std::vector<LandmarkObs> &observations, const Map &map_landmarks) {
-	// TODO: Update the weights of each particle using a mult-variate Gaussian distribution. You can read
-	//   more about this distribution here: https://en.wikipedia.org/wiki/Multivariate_normal_distribution
-	// NOTE: The observations are given in the VEHICLE'S coordinate system. Your particles are located
-	//   according to the MAP'S coordinate system. You will need to transform between the two systems.
-	//   Keep in mind that this transformation requires both rotation AND translation (but no scaling).
-	//   The following is a good resource for the theory:
-	//   https://www.willamette.edu/~gorr/classes/GeneralGraphics/Transforms/transforms2d.htm
-	//   and the following is a good resource for the actual equation to implement (look at equation 
-	//   3.33
-	//   http://planning.cs.uiuc.edu/node99.html
 
 	for (int i = 0; i < particles.size(); i++) {
-		std::vector<LandmarkObs> observations_transf;
+		std::vector<LandmarkObs> observations_transf;		
 		Particle p = particles[i];
 		for (LandmarkObs obs : observations) {
 			LandmarkObs trans_obj;
@@ -152,22 +126,22 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 			associations.push_back(association);
 			sense_x.push_back(t_obs.x);
 			sense_y.push_back(t_obs.y);
-		}	
+		}
 
-		SetAssociations(p, associations, sense_x, sense_y);			
+		// reassign p back to particles
+		particles[i] = p;
+		// SetAssociations(particles[i], associations, sense_x, sense_y);
+			
 	}
 }
 
 void ParticleFilter::resample() {
-	// TODO: Resample particles with replacement with probability proportional to their weight. 
-	// NOTE: You may find std::discrete_distribution helpful here.
-	//   http://en.cppreference.com/w/cpp/numeric/random/discrete_distribution
 
 	std::vector<Particle> resampling_particles;
 	std::default_random_engine gen;
 	for(int i=0; i< num_particles; i++) {
-		std::discrete_distribution<int> index(weights.begin(), weights.end());
-		resampling_particles[i] = particles[index(gen)];
+		std::discrete_distribution<int> index(weights.begin(), weights.end());		
+		resampling_particles.push_back(particles[index(gen)]);
 	}
 
 	// replace current particles
